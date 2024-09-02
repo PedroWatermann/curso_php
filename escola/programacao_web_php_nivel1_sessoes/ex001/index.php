48fe5bf2551836a5d10db2933db47b34c3dfcd62
PedroWatermann <pedro.programmer.pt@gmail.com>
2024-09-02 12:01:44 -0300

    ex001 finalizado

--- escola/programacao_web_php_nivel1_sessoes/ex001/index.php
+++ escola/programacao_web_php_nivel1_sessoes/ex001/index.php
@@ -10,7 +10,7 @@ $_SESSION["pass"] = "12345";
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Login</title>
     </head>
 
     <body>
--- escola/programacao_web_php_nivel1_sessoes/ex001/login.php
+++ escola/programacao_web_php_nivel1_sessoes/ex001/login.php
@@ -1,14 +1,42 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 
 <body>
-
+    <header>
+        <h1>Login</h1>
+    </header>
+    
+    <main>
+        <?php
+            if (isset($_POST["usuario"]) && isset($_POST["senha"])) 
+            {
+                $usua = $_POST["usuario"] ?? "";
+                $senh = $_POST["senha"] ?? "";
+                if ($usua == $_SESSION["user"] && $senh == $_SESSION["pass"])
+                {
+                    echo "<p>Usuário logado com sucesso!</p>";
+                }
+                else 
+                {
+                    echo "<p>Usuário não cadastrado.</p>";
+                    echo "
+                        <script>
+                            history.go(index.php);
+                        </script>";
+                }
+            }
+        ?>
+    </main>
 </body>
 
 </html>
